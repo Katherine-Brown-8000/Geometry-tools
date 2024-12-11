56aaf13289e4c6a553e336786a37812751e2d8cd
Katherine Brown <155707632+Katherine-Brown-8000@users.noreply.github.com>
2024-12-10 20:42:34 -0500

    Update Area of a Circle.py

--- Area Equation/Area of a Circle.py
+++ Area Equation/Area of a Circle.py
@@ -3,5 +3,6 @@
 
 radius = float(input("Enter the radius of your circle: "))
 pie = 3.14
-sum = 2 * pie * radius
+sqaure = radius * radius
+sum = sqaure * pie
 print(f"The area of your circle is {sum}")
